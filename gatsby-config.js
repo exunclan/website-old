326cdea73d571473b935f011d8754c75b00b0779
Somesh Kar <somesh.kar@gmail.com>
2020-02-16 21:53:36 +0530

    Change theme and background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#298bf5',
+        theme_color: '#298bf5',
         display: 'minimal-ui',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
