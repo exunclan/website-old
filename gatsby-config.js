3cdacc30db29bd22c7fec729da06f2f75453c55e
Somesh Kar <14039437+someshkar@users.noreply.github.com>
2020-04-19 18:18:35 +0530

    Add Google Analytics (#8)
    Added gatsby-plugin-google-analytics with tracking code UA-163988640-1

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,13 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-163988640-1',
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
