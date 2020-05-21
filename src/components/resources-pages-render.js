3c2c72be341ea89cd9bea5611b366e08de694020
Akshit Kumar <41486636+akshitkr@users.noreply.github.com>
2020-05-21 12:39:12 +0530

    Added resources page (#17)

---
+++ src/components/resources-pages-render.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import pages from '../components/resources-list';
+
+const linkstyle = {
+  color: '#298bf5',
+};
+
+const Link = (props) => {
+  return (
+    <a href={props.page} style={linkstyle}>
+      {props.name}
+    </a>
+  );
+};
+
+const ResourceList = () => {
+  return pages.map((page, index) => {
+    if (index == pages.length - 1) {
+      return (
+        <span style={{ display: 'inline-block' }} key={index}>
+          and&nbsp;
+          <Link name={page.name} page={page.url} />.
+        </span>
+      );
+    } else {
+      return (
+        <span style={{ display: 'inline-block' }} key={index}>
+          <Link name={page.name} page={page.url} />
+          ,&nbsp;
+        </span>
+      );
+    }
+  });
+};
+
+export default ResourceList;
---
+++ src/pages/resources.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Layout from '../components/layout';
+import Container from '../components/container';
+import Navbar from '../components/navbar';
+import SEO from '../components/seo';
+
+import ResourceList from '../components/resources-pages-render';
+
+const Grid = ({ children }) => <div className={styles.grid}>{children}</div>;
+Grid.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const linkstyle = {
+  color: '#298bf5',
+};
+
+const ResourcesPage = () => (
+  <Layout>
+    <SEO title="Resources" />
+    <Container>
+      <Navbar />
+      <div
+        style={{
+          padding: '10rem 0 5rem',
+        }}
+      >
+        <h1>Learning Resources</h1>
+        <p>
+          An archive of resources assembled by current Exun members and alumni
+          to help newcomers navigate the fields of&nbsp;
+          {ResourceList()} Before you begin, you may find it useful to learn{' '}
+          <a
+            href="https://docs.google.com/document/d/1ON_SZS_0msRgfo8sB7jjtHDztXiqV17c8TmW22q-MSI/edit?usp=sharing"
+            style={linkstyle}
+          >
+            how to learn
+          </a>
+          .
+        </p>
+        <p>
+          Let us know if you like the resources{' '}
+          <a
+            href="https://docs.google.com/forms/d/1awwX8ejmJE6Hvf4sfTyPI7weqZAYzYycnzFrCkygE4g/edit?usp=sharing"
+            style={linkstyle}
+          >
+            here
+          </a>
+          .
+        </p>
+      </div>
+    </Container>
+  </Layout>
+);
+
+export default ResourcesPage;
