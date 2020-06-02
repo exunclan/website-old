126750aeec4c52a04b8d05ef5fa892272ab6d8cb
Angad Singh <mail@angad.dev>
2020-06-02 22:52:06 +0530

    Add line breaks

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -20,12 +20,14 @@ export default () => (
           Thank you for registering! We will be e-mailing invitations to the
           Google Classroom(s) for the department(s) you have registered for,
           within 24 hours. All announcements and details regarding the
-          inductions for that department will be posed in the classroom. If you
-          wish to participate in another department and did not select it in the
-          form, or if you have any other questions, please email us at
-          exun@dpsrkp.net. Looking forward to having you with us for e-Lite
-          2020!
+          inductions for that department will be posed in the classroom.
         </p>
+        <p>
+          If you wish to participate in another department and did not select it
+          in the form, or if you have any other questions, please email us at
+          exun@dpsrkp.net.
+        </p>
+        <p>Looking forward to having you with us for e-Lite 2020!</p>
       </div>
     </Container>
   </Layout>
