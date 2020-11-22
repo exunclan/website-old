da340e453a75ecf2d8bebd6fcf5dd5b1f29768e8
Samik Tuteja <samiktuteja2008@gmail.com>
2020-11-22 10:14:39 +0530

    Added fireside chat

---
+++ data/firesides.js
@@ -0,0 +1,24 @@
+const firesides = [
+  {
+    img: 'saif.jpeg',
+    name: 'Saif Hasan',
+    post: "Exun President '95",
+    work: 'Engineering Manager',
+    company: 'Facebook, Seattle',
+  },
+  {
+    img: 'gaurav.jpeg',
+    name: 'Gaurav Lochan',
+    post: "Exun President '95",
+    work: 'Engineering Manager',
+    company: 'WhatsApp, Palo Alto',
+  },
+  {
+    img: 'manas.jpeg',
+    name: 'Saif Hasan',
+    post: "Exun President '01",
+    work: 'Head of Product (Ads)',
+    company: 'Uber, San Francisco',
+  },
+];
+export default firesides;
