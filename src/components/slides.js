d7f7483eef43a22d654b6d6fc067632c50cd97c0
Kavin Desi Valli <kavinvalli@gmail.com>
2020-11-28 18:26:52 +0530

    changed function to component

--- src/components/slides.js
+++ src/components/slides.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import ReactGoogleSlides from 'react-google-slides';
 
-function Slides() {
-  const isBrowser = typeof window !== `undefined`;
-  if (isBrowser) {
-    return (
-      <div>
-        <ReactGoogleSlides
-          width="800rem"
-          height="480rem"
-          slidesLink="https://docs.google.com/presentation/d/11Pb7aDHt38LydehkRdkVrqM-t389YLpISmbkh7H0_Is/edit?usp=sharing"
-          showControls
-        />
-      </div>
-    );
+export default class Slides extends Component {
+  render() {
+    const isBrowser = typeof window !== `undefined`;
+    if (isBrowser) {
+      return (
+        <div>
+          <ReactGoogleSlides
+            width="800rem"
+            height="480rem"
+            slidesLink="https://docs.google.com/presentation/d/11Pb7aDHt38LydehkRdkVrqM-t389YLpISmbkh7H0_Is/edit?usp=sharing"
+            showControls
+          />
+        </div>
+      );
+    }
+    return null;
   }
-  return null;
 }
 export default Slides;
