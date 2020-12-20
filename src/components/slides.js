f7bd779f195b9374f5e542cbae59c5c9895894ef
Samik Tuteja <samiktuteja2008@gmail.com>
2020-12-20 15:33:07 +0530

    Made responsive

--- src/components/slides.js
+++ src/components/slides.js
@@ -5,8 +5,8 @@ export default function Slides() {
     <iframe
       src="https://docs.google.com/presentation/d/e/2PACX-1vR7Wz5b3QAtV031bPPuWU59KTgdEbR-nFDYuN0PDGqUzM31mngWOAnIQKesOobvabb2LeyJgLIi0pSi/embed?start=false&loop=false&delayms=5000"
       frameborder="0"
-      width="800"
-      height="480"
+      width="90%"
+      height="490"
       allowfullscreen="true"
       mozallowfullscreen="true"
       webkitallowfullscreen="true"
