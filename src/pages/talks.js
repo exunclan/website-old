d02b8220a1b692b8ec5cd7b033b31b0c416ba8c3
Samik-coder <e10371samik@dpsrkp.net>
2022-01-06 22:57:57 +0530

    added fireside

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '../components/container';
+import Fireside from '../components/fireside';
 import Layout from '../components/layout';
 import Navbar from '../components/navbar';
 import Talk from '../components/talk'
@@ -12,7 +13,9 @@ class Talks extends React.Component {
               <Container>
                 <Navbar />
                 <div style={{marginTop: "7rem"}}></div>
+                <Fireside />
                 <Talk />
+                <Fireside />
               </Container>
           </Layout>
       </div>
