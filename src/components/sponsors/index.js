2866acf844ca7b931d9f93994505c2779219d1cb
Kavin Valli <kavinvalli@gmail.com>
2022-01-06 19:53:33 +0530

    chore: update sponsors for 2021-22

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import IvyAchievement from './ivyachievement.png';
+
+import Plaksha from './plaksha.png';
+import WolframLogo from './wolfram.png';
+import DevfolioLogo from './devfolio.png';
+// import AthenaLogo from './athena.png';
+import AOPSLogo from './aops.png';
+import WwcDelhiLogo from './wwc.jpeg';
+import TaskadeLogo from './taskade.png';
 
 const logoStyles = {
-  height: '60px',
+  height: '150px',
   width: 'auto',
 };
 
@@ -10,12 +17,26 @@ export default () => (
   <>
     <div
       style={{
-        margin: '1em 0',
+        // borderTop: '1px solid #eaeaef',
+        margin: '3em 0',
+        // padding: '3em 0',
         paddingBottom: '',
-        display: 'flex',
-        justifyContent: 'center',
       }}
     >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <img alt="Plaksha" src={Plaksha} style={logoStyles} />
+        {/* <img alt="Athena" src={AthenaLogo} style={logoStyles} /> */}
+        {/* <img alt="Digital India" src={DigitalIndia} style={logoStyles} /> */}
+      </div>
       <div
         style={{
           maxWidth: '900px',
@@ -34,7 +55,7 @@ export default () => (
               marginTop: '30px',
             }}
           >
-            Powered by
+            Community Partners
           </div>
           <div
             style={{
@@ -42,16 +63,91 @@ export default () => (
               justifyContent: 'space-around',
               alignItems: 'center',
               flexWrap: 'wrap',
-              maxWidth: '500px',
-              width: '100%',
-              padding: '0 15px',
             }}
           >
             <img
-              alt="Ivy Achievement"
-              src={IvyAchievement}
-              style={{ height: 'auto', width: '100%' }}
+              alt="Wolfram"
+              src={WolframLogo}
+              style={{ height: '70px', width: 'auto' }}
             />
+            <img
+              alt="Women Who Code Delhi"
+              src={WwcDelhiLogo}
+              style={{ height: '70px', width: 'auto' }}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <div
+              style={{
+                fontSize: '30px',
+                marginBottom: '30px',
+                textAlign: 'center',
+                marginTop: '30px',
+              }}
+            >
+              Hackathon Partner
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+              }}
+            >
+              <img
+                alt="Devfolio"
+                src={DevfolioLogo}
+                style={{ height: '50px', width: 'auto' }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          maxWidth: '900px',
+          margin: '30px auto',
+          display: 'flex',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+        }}
+      >
+        <div>
+          <div>
+            <div
+              style={{
+                fontSize: '30px',
+                marginBottom: '30px',
+                textAlign: 'center',
+                marginTop: '30px',
+              }}
+            >
+              Other Partners
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+              }}
+            >
+              <img
+                alt="AOPS"
+                src={AOPSLogo}
+                style={{ height: '50px', width: 'auto' }}
+              />
+              <a href="https://taskade.com" target="_blank" rel="noreferrer">
+                <img
+                  alt="Taskade"
+                  src={TaskadeLogo}
+                  style={{ height: '50px', width: 'auto' }}
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
