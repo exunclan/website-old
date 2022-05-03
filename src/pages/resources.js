1ebd666d2604aa402a42b92337a7da00fe10f6a4
Darsh Sikka <darshsikka@Darshs-Air.Dlink>
2022-05-03 18:54:37 +0530

    redirect resources to learning

---
+++ src/pages/resources.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import useScript from '../components/useScript'
+const resources=()=>{
+    useScript('/redirect.js')
+    return <div>
+    </div>
+}
+export default resources;
